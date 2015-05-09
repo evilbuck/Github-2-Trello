0174b0b9476944b31de7631f947d11a4ff12f0c4
Evil Buck <buckley@brightbytes.net>
2015-05-09 10:05:52 -0700

    refactor pullRequest eventHandler
    - handling closed pull_requests
    - refactored pullRequest event_handler to handle more than one action
      [closed, opened]
    
    [Fixes #tnpKiDKO]

--- config/initializers/trello.js
+++ config/initializers/trello.js
@@ -1,3 +1,4 @@
+/// <reference path="../../typings/node/node.d.ts"/>
 var Trello = require('node-trello');
 var token  = process.env.TRELLO_TOKEN;
 var key    = process.env.TRELLO_DEVELOPER_PUBLIC_KEY;
@@ -5,5 +6,6 @@ var key    = process.env.TRELLO_DEVELOPER_PUBLIC_KEY;
 var t  = new Trello(key, token);
 
 t.PR_LIST_ID = process.env.TRELLO_PR_LIST_ID;
+t.MERGE_LIST_ID = process.env.TRELLO_MERGE_LIST_ID;
 
 module.exports = t;
---
+++ event_handlers/pull_request/opened.js
@@ -0,0 +1,71 @@
+var Github = require('github');
+var q      = require('q');
+var githubToken = process.env.GITHUB_TOKEN;
+
+var t = require('../../config/initializers/trello');
+var prListId = t.PR_LIST_ID;
+
+var gh = new Github({
+  version: '3.0.0',
+  debug: true
+});
+
+function parseCommit(commitMessage) {
+  var trelloCardId;
+  var trelloIdRegex = /\[fixes #([\w\-]+)\]/im;
+
+  if (trelloIdRegex.test(commitMessage)) {
+    // TODO: handle multiple cards in one commit
+    trelloCardId = commitMessage.match(trelloIdRegex)[1];
+    return trelloCardId;
+  }
+
+  return null;
+}
+
+function authenticateGithub() {
+  gh.authenticate({
+    type: 'oauth',
+    token: githubToken
+  });
+}
+
+function PullRequest(data) {
+  var trelloCardId;
+  var sha;
+
+  debugger
+  sha = data.pull_request.base.sha;
+
+  var params = {
+    user: data.pull_request.user.login,
+    repo: data.repository.name,
+    number: data.pull_request.number
+  };
+  
+  authenticateGithub();
+  // get commits and commit messages
+  return q.nfcall(gh.pullRequests.getCommits.bind(gh.pullRequests), params).then(function(ghRes) {
+    // TODO: find multiple instances of the [Fixes #<id>] syntax
+    var trelloCards = ghRes.reduce(function(memo, prCommit) {
+      var cardId = parseCommit(prCommit.commit.message);
+      if (cardId !== null) {
+        memo.push(cardId);
+      }
+
+      return memo;
+    }, []);
+
+    return trelloCards;
+  }, function(err) { console.error("things fucked up:", err, params); })
+  .then(function(commits) {
+    // move all trello cards to Pull Requested list
+    var moveCards = commits.map(function(trelloCardId) {
+      return q.nfcall(t.put.bind(t), '/1/cards/' + trelloCardId + '/idList', { value: prListId });
+    });
+
+    return q.all(moveCards);
+  });
+}
+
+module.exports = PullRequest;
